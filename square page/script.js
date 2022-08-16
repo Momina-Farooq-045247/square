24523c4943f5fe0d1d8ee70027e484aee2a50472
Momina Farooq <107984079+Momina-Farooq-045247@users.noreply.github.com>
2022-08-15 23:00:58 -0700

    Add files via upload

---
+++ square page/script.js
@@ -0,0 +1,10 @@
+function fun(){
+    console.log('getting square')
+    let v = document.querySelector ('input').value
+     document.querySelector('h1').innerText = `Square is =  ${v*v}  `
+
+
+
+   //alert ('Square will be '+ v*v )
+   
+}
